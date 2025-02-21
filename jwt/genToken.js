f90b00e5da708d0209da5f65d3f76ffd9d33d4d2
MarkC1-code <marksimon5093@gmail.com>
2025-02-21 19:44:20 +0100

    added Semicolon to a function

--- jwt/genToken.js
+++ jwt/genToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const genToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: '20m'})
-}
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '20m' })
+};
 
 module.exports = genToken
